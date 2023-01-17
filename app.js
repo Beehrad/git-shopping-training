b79fe2bd2d9a0227c67e2f7bd3bc6e2a85e00810
Beehrad <beehrad@yahoo.com>
2023-01-17 21:24:35 +0330

    closeModal js updated

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const modalBtn = document.querySelector(".charkh")
 const modal = document.querySelector(".modal")
 const backdrop = document.querySelector(".backdrop")
+const confirmBtn = document.querySelector(".confirm")
 
 
 modalBtn.addEventListener("click" , ()=>{
     backdrop.style.display = "flex"
     modal.style.transform = "translateY(0vh)";
     modal.style.opacity = "1"
+})
+
+confirmBtn.addEventListener("click" , ()=>{
+    backdrop.style.display = "none"
+    modal.style.transform = "translateY(-100vh)";
+    modal.style.opacity = "0"
 })
