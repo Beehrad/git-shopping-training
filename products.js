780273f479bddf0cdd644f5638ad248db55dc384
Beehrad <beehrad@yahoo.com>
2023-01-18 15:28:45 +0330

    products.js updated

--- products.js
+++ products.js
@@ -1,51 +1,51 @@
-export const productsData{
+export const productsData =[
     {
-        id = 1;
-        title = "Double bed"
-        price = 10.99
-        imageUrl = "./img/products-1.jpg"
+        id : 1,
+        title : "Double bed",
+        price : 10.99,
+        imageUrl : "./img/products-1.jpg",
     },
     {
-        id = 2;
-        title = "Queen bed"
-        price = 13.99
-        imageUrl = "./img/products-2.jpg"
+        id : 2,
+        title : "Queen bed",
+        price : 13.99,
+        imageUrl : "./img/products-2.jpg",
     },
     {
-        id = 3;
-        title = "Wood bed"
-        price = 23.99
-        imageUrl = "./img/products-3.jpg"
+        id : 3,
+        title : "Wood bed",
+        price : 23.99,
+        imageUrl : "./img/products-3.jpg",
     },
     {
-        id = 4;
-        title = "Stolz bed"
-        price = 19.99
-        imageUrl = "./img/products-4.jpg"
+        id : 4,
+        title : "Stolz bed",
+        price : 19.99,
+        imageUrl : "./img/products-4.jpg",
     },
     {
-        id = 5;
-        title = "Dream bed"
-        price = 16.99
-        imageUrl = "./img/products-5.jpg"
+        id : 5,
+        title : "Dream bed",
+        price : 16.99,
+        imageUrl : "./img/products-5.jpg",
     },
     {
-        id = 6;
-        title = "Multi bed"
-        price = 28.99
-        imageUrl = "./img/products-6.jpg"
+        id : 6,
+        title : "Multi bed",
+        price : 28.99,
+        imageUrl : "./img/products-6.jpg",
     },
     {
-        id = 7;
-        title = "WiteWood bed"
-        price = 12.99
-        imageUrl = "./img/products-7.jpg"
+        id : 7,
+        title : "WiteWood bed",
+        price : 12.99,
+        imageUrl : "./img/products-7.jpg",
     },
     {
-        id = 8;
-        title = "Passenger bed"
-        price = 15.99
-        imageUrl = "./img/products-8.jpg"
+        id : 8,
+        title : "Passenger bed",
+        price : 15.99,
+        imageUrl : "./img/products-8.jpg",
     },
 
-}
+];
